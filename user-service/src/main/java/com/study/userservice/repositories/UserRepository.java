55398f800f7e7b53bb639df3de99b9d4230eb252
Test_repo <22520992@gm.uit.edu.vn>
2025-03-14 11:03:54 +0700

    Configs modelMapper and implements User service

--- user-service/src/main/java/com/study/userservice/repositories/UserRepository.java
+++ user-service/src/main/java/com/study/userservice/repositories/UserRepository.java
@@ -1,10 +1,14 @@
 package com.study.userservice.repositories;
 
 import com.study.userservice.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByUsername(String username);
+    Page<User> findByUsernameContaining(String username, Pageable pageable);
 }
--- user-service/src/main/java/com/study/userservice/services/UserServiceImpl.java
+++ user-service/src/main/java/com/study/userservice/services/UserServiceImpl.java
@@ -1,10 +1,15 @@
 package com.study.userservice.services;
 
+import com.study.userservice.domain.User;
 import com.study.userservice.dto.request.CreateUserRequest;
 import com.study.userservice.dto.request.UpdateUserRequest;
 import com.study.userservice.dto.response.UserResponse;
+import com.study.userservice.exceptions.BusinessException;
+import com.study.userservice.exceptions.NotFoundException;
 import com.study.userservice.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,34 +21,61 @@ import java.util.UUID;
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     public UserResponse createUser(CreateUserRequest request) {
-//        if(userRepository.existsByUsername(request.getUsername()))
-//            throw new ValidateException("Duplicate username");
+        if(userRepository.existsByUsername(request.getUsername()))
+            throw new BusinessException("Username already exists");
 
+        User user = User.builder()
+                .username(request.getUsername())
+                .dateOfBirth(request.getDateOfBirth())
+                .gender(request.getGender())
+                .build();
 
+        User savedUser = userRepository.save(user);
 
-        return null;
+        return modelMapper.map(savedUser, UserResponse.class);
     }
 
     @Override
     public UserResponse getUserById(UUID id) {
-        return null;
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new NotFoundException("User with id " + id + " not found")
+        );
+
+        return modelMapper.map(user, UserResponse.class);
     }
 
     @Override
-    public UserResponse updateUser(UUID id, UpdateUserRequest user) {
-        return null;
+    public UserResponse updateUser(UUID id, UpdateUserRequest request) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new NotFoundException("User with id " + id + " not found")
+        );
+
+        //Check if username has already existed, ignore the case username is unchanged.
+        if(request.getUsername() != null &&
+               !request.getUsername().equals(user.getUsername()) &&
+               userRepository.existsByUsername(request.getUsername()))
+           throw new BusinessException("Username already exists");
+
+       modelMapper.map(request, user);
+
+       userRepository.save(user);
+
+       return modelMapper.map(user, UserResponse.class);
     }
 
     @Override
     public Page<UserResponse> searchUserByUsername(String keyword, Pageable pageable) {
-        return null;
+        Page<User> users = userRepository.findByUsernameContaining(keyword, pageable);
+        return users.map(user -> modelMapper.map(user, UserResponse.class));
     }
 
     @Override
     public List<UserResponse> getUsersByListOfIds(List<UUID> ids) {
-        return List.of();
+        List<User> users = userRepository.findAllById(ids);
+        return modelMapper.map(users, new TypeToken<List<UserResponse>>() {}. getType());
     }
 }
