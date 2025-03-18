42ef9944e2599779b47dd5f02aea01f897a4b1e2
Test_repo <22520992@gm.uit.edu.vn>
2025-03-18 10:29:39 +0700

    Add common_proto project

---
+++ common-proto/src/main/java/com/study/commonproto/CommonProtoApplication.java
@@ -0,0 +1,13 @@
+package com.study.commonproto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonProtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonProtoApplication.class, args);
+    }
+
+}
---
+++ common-proto/src/test/java/com/study/commonproto/CommonProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.commonproto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonProtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
