0833b9ce3d4995a428f7063f7b77162dac089cae
Test_repo <22520992@gm.uit.edu.vn>
2025-03-23 00:46:12 +0700

    Add repositories and team-service proto file

---
+++ team-service/src/main/java/com/study/teamservice/repository/TeamMemberRepository.java
@@ -0,0 +1,9 @@
+package com.study.teamservice.repository;
+
+import com.study.teamservice.entity.TeamMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TeamMemberRepository extends JpaRepository<TeamMember, UUID> {
+}
---
+++ team-service/src/main/java/com/study/teamservice/repository/TeamRepository.java
@@ -0,0 +1,9 @@
+package com.study.teamservice.repository;
+
+import com.study.teamservice.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TeamRepository extends JpaRepository<Team, UUID> {
+}
