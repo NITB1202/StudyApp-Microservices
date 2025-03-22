e5482f6bd01e7a69003999a92732701570495bdb
Test_repo <22520992@gm.uit.edu.vn>
2025-03-22 23:18:48 +0700

    Add Team and Team member entity

---
+++ common/src/main/java/com/study/common/enums/TeamRole.java
@@ -0,0 +1,5 @@
+package com.study.common.enums;
+
+public enum TeamRole {
+    CREATOR, ADMIN, MEMBER
+}
---
+++ team-service/src/main/java/com/study/teamservice/entity/Team.java
@@ -0,0 +1,38 @@
+package com.study.teamservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "teams")
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name= "team_code", nullable = false, unique = true)
+    private String teamCode;
+
+    @Column(name = "create_date", nullable = false)
+    private LocalDate createDate;
+
+    @Column(name = "creator_id", nullable = false)
+    private UUID creatorId;
+
+    @Column(name = "total_members", nullable = false)
+    private Integer totalMembers;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+}
