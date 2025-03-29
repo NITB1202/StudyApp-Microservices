30afff0e039c91300dc32d0161fad2d2be856b40
Test_repo <22520992@gm.uit.edu.vn>
2025-03-29 18:26:06 +0700

    Add sample data

---
+++ team-service/src/main/resources/database/data.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS teams (
+    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    team_code VARCHAR(10) NOT NULL UNIQUE,
+    create_date DATE NOT NULL,
+    creator_id UUID NOT NULL,
+    total_members INT DEFAULT 1 NOT NULL,
+    avatar_url VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS team_user (
+    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    team_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    join_date DATE NOT NULL,
+    role VARCHAR(20) NOT NULL,
+    CONSTRAINT unique_team_user UNIQUE (user_id, team_id)
+);
