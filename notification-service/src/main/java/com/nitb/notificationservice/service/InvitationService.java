a9bd1b3cbbf9a02d964a17a409698414b0f11112
Test_repo <22520992@gm.uit.edu.vn>
2025-06-11 10:18:05 +0700

    refactor: update services

--- notification-service/src/main/java/com/nitb/notificationservice/service/InvitationService.java
+++ notification-service/src/main/java/com/nitb/notificationservice/service/InvitationService.java
@@ -1,6 +1,7 @@
 package com.nitb.notificationservice.service;
 
 import com.nitb.notificationservice.entity.Invitation;
+import com.study.notificationservice.grpc.GetInvitationsRequest;
 import com.study.notificationservice.grpc.ReplyToInvitationRequest;
 
 import java.util.List;
@@ -8,6 +9,6 @@ import java.util.UUID;
 
 public interface InvitationService {
     void createInvitation(UUID inviterId, UUID inviteeId, UUID teamId);
-    List<Invitation> getRecentInvitations(UUID inviteeId, int page, int size);
+    List<Invitation> getInvitations(GetInvitationsRequest request);
     void replyToInvitation(ReplyToInvitationRequest request);
 }
--- notification-service/src/main/java/com/nitb/notificationservice/service/NotificationService.java
+++ notification-service/src/main/java/com/nitb/notificationservice/service/NotificationService.java
@@ -4,13 +4,14 @@ import com.nitb.notificationservice.dto.CreateNotificationRequestDto;
 import com.nitb.notificationservice.entity.Notification;
 import com.study.notificationservice.grpc.DeleteAllNotificationsRequest;
 import com.study.notificationservice.grpc.DeleteNotificationsRequest;
-import com.study.notificationservice.grpc.GetNotificationRequest;
+import com.study.notificationservice.grpc.GetNotificationsRequest;
 import com.study.notificationservice.grpc.MarkAllNotificationsAsReadRequest;
-import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public interface NotificationService {
     void createNotification(CreateNotificationRequestDto request);
-    Page<Notification> getNotifications(GetNotificationRequest request);
+    List<Notification> getNotifications(GetNotificationsRequest request);
     void markNotificationAsRead(MarkAllNotificationsAsReadRequest request);
     void markAllNotificationsAsRead(MarkAllNotificationsAsReadRequest request);
     void deleteNotifications(DeleteNotificationsRequest request);
