df5cdca41d6eef965dffd77b52155325ec74ecc5
Test_repo <22520992@gm.uit.edu.vn>
2025-06-15 20:46:04 +0700

    feat: add sample data for team notification settings service

--- notification-service/src/main/resources/database/data.sql
+++ notification-service/src/main/resources/database/data.sql
@@ -21,3 +21,10 @@ INSERT INTO device_tokens (
 ) VALUES
     ('eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee', '9f5d79f2-83a2-411d-bb66-caeb640a62b0',
      'fPosfcSXTKCtb1agcJ8tHI:APA91bFRp_1U_lEnlXMGrpgqR1FwfQF61JYDV43d1qonzumIQvzuX_IHgTCGZUjI0RBXCRToJOpZ3RaA_O5dndYFfFM1v8aNdgnQaw6KiRYPY3etPrJKngc', NOW());
+
+INSERT INTO team_notification_settings (
+    id, user_id, team_id, team_notification, team_plan_reminder, chat_notification
+) VALUES
+      ('10000000-0000-0000-0000-000000000001', '041c77e0-ae77-4005-b745-ea12dca9bec6', '111e8400-e29b-41d4-a716-446655440001', true, true, true),
+      ('10000000-0000-0000-0000-000000000002', '618c10ee-923f-4323-b32b-086caa534b46', '111e8400-e29b-41d4-a716-446655440001', true, false, true),
+      ('10000000-0000-0000-0000-000000000003', '9f5d79f2-83a2-411d-bb66-caeb640a62b0', '555e8400-e29b-41d4-a716-446655440006', false, false, false);
