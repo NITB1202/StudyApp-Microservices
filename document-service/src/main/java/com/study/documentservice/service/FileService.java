6176ddd08bf66938b396ed6d4ac8baa3ba0dfd97
Test_repo <22520992@gm.uit.edu.vn>
2025-06-16 11:10:15 +0700

    feat: implement file service

--- document-service/src/main/java/com/study/documentservice/service/FileService.java
+++ document-service/src/main/java/com/study/documentservice/service/FileService.java
@@ -4,6 +4,6 @@ import com.google.protobuf.ByteString;
 
 public interface FileService {
     String uploadFile(String folderPath, String publicId, ByteString byteString);
-    void moveFile(String publicId, String newPublicId);
+    void moveFile(String publicId, String newFolder);
     void deleteFile(String publicId);
 }
---
+++ document-service/src/main/java/com/study/documentservice/service/impl/FileServiceImpl.java
@@ -0,0 +1,57 @@
+package com.study.documentservice.service.impl;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import com.google.protobuf.ByteString;
+import com.study.common.exceptions.BusinessException;
+import com.study.documentservice.service.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class FileServiceImpl implements FileService {
+    private final Cloudinary cloudinary;
+
+    @Override
+    public String uploadFile(String folderPath, String publicId, ByteString byteString) {
+        byte[] bytes = byteString.toByteArray();
+
+        Map params = ObjectUtils.asMap(
+                "resource_type", "auto",
+                "public_id", publicId,
+                "asset_folder", folderPath,
+                "overwrite", "true"
+        );
+
+        try {
+            Map result = cloudinary.uploader().upload(bytes, params);
+            return result.get("secure_url").toString();
+        }
+        catch (IOException e) {
+            throw new BusinessException("Error while uploading file.");
+        }
+    }
+
+    @Override
+    public void moveFile(String publicId, String newFolder) {
+        try {
+            cloudinary.api().update(publicId,ObjectUtils.asMap("asset_folder", newFolder));
+        } catch (Exception e) {
+            throw new BusinessException("Error while moving file.");
+        }
+    }
+
+    @Override
+    public void deleteFile(String publicId) {
+        try {
+            cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+        }
+        catch (IOException e) {
+            throw new BusinessException("Error while deleting file.");
+        }
+    }
+}
