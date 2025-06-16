ef9dc5a9c26ea5f6a4e5dd8fdf7679fa63930bd7
Test_repo <22520992@gm.uit.edu.vn>
2025-06-16 08:27:55 +0700

    feat: add proto file

--- document-service/src/main/java/com/study/documentservice/DocumentServiceApplication.java
+++ document-service/src/main/java/com/study/documentservice/DocumentServiceApplication.java
@@ -2,8 +2,10 @@ package com.study.documentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.study.documentservice", "com.study.common"})
+@EnableDiscoveryClient
 public class DocumentServiceApplication {
 
     public static void main(String[] args) {
