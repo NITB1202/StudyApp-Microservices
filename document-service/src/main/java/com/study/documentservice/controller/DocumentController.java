09ab265281153bc3cc3db6f537e60b2d09af39ea
Test_repo <22520992@gm.uit.edu.vn>
2025-06-16 12:40:09 +0700

    feat: complete document controller

--- document-service/src/main/java/com/study/documentservice/controller/DocumentController.java
+++ document-service/src/main/java/com/study/documentservice/controller/DocumentController.java
@@ -1,12 +1,17 @@
 package com.study.documentservice.controller;
 
 import com.study.common.grpc.ActionResponse;
+import com.study.documentservice.dto.FileResponseDto;
+import com.study.documentservice.entity.Document;
 import com.study.documentservice.entity.Folder;
 import com.study.documentservice.entity.TeamUsage;
 import com.study.documentservice.entity.UserUsage;
 import com.study.documentservice.grpc.*;
+import com.study.documentservice.mapper.DocumentMapper;
 import com.study.documentservice.mapper.FolderMapper;
 import com.study.documentservice.mapper.UsageMapper;
+import com.study.documentservice.service.DocumentService;
+import com.study.documentservice.service.FileService;
 import com.study.documentservice.service.FolderService;
 import com.study.documentservice.service.UsageService;
 import io.grpc.stub.StreamObserver;
@@ -14,12 +19,15 @@ import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 import java.util.List;
+import java.util.UUID;
 
 @GrpcService
 @RequiredArgsConstructor
 public class DocumentController extends DocumentServiceGrpc.DocumentServiceImplBase {
     private final UsageService usageService;
     private final FolderService folderService;
+    private final DocumentService documentService;
+    private final FileService fileService;
 
     //Usage
     @Override
@@ -96,7 +104,9 @@ public class DocumentController extends DocumentServiceGrpc.DocumentServiceImplB
 
     @Override
     public void deleteFolder(DeleteFolderRequest request, StreamObserver<ActionResponse> responseObserver) {
+        UUID id = UUID.fromString(request.getId());
         folderService.deleteFolder(request);
+        documentService.deleteAllDocuments(id);
 
         ActionResponse response = ActionResponse.newBuilder()
                 .setSuccess(true)
@@ -110,42 +120,99 @@ public class DocumentController extends DocumentServiceGrpc.DocumentServiceImplB
     //Document
     @Override
     public void uploadDocument(UploadDocumentRequest request, StreamObserver<DocumentResponse> responseObserver) {
-
+        Document document = documentService.uploadDocument(request);
+        DocumentResponse response = DocumentMapper.toDocumentResponse(document);
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 
     @Override
     public void getDocumentById(GetDocumentByIdRequest request, StreamObserver<DocumentDetailResponse> responseObserver) {
-
+        Document document = documentService.getDocumentById(request);
+        DocumentDetailResponse response = DocumentMapper.toDocumentDetailResponse(document);
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 
     @Override
     public void getDocuments(GetDocumentsRequest request, StreamObserver<DocumentsResponse> responseObserver) {
+        UUID folderId = UUID.fromString(request.getFolderId());
 
+        List<Document> documents = documentService.getDocuments(request);
+        long total = documentService.getDocumentCount(folderId);
+        String nextCursor = !documents.isEmpty() && documents.size() == request.getSize() ?
+                documents.get(documents.size() - 1).getCreatedAt().toString() : "";
+
+        DocumentsResponse response = DocumentMapper.toDocumentsResponse(documents, total, nextCursor);
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 
     @Override
     public void searchDocumentByName(SearchDocumentByNameRequest request, StreamObserver<DocumentsResponse> responseObserver) {
+        UUID folderId = UUID.fromString(request.getFolderId());
 
+        List<Document> documents = documentService.searchDocumentByName(request);
+        long total = documentService.getDocumentCountWithKeyword(folderId, request.getKeyword());
+        String nextCursor = !documents.isEmpty() && documents.size() == request.getSize() ?
+                documents.get(documents.size() - 1).getCreatedAt().toString() : "";
+
+        DocumentsResponse response = DocumentMapper.toDocumentsResponse(documents, total, nextCursor);
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 
     @Override
     public void updateDocumentName(UpdateDocumentNameRequest request, StreamObserver<ActionResponse> responseObserver) {
+        documentService.updateDocumentName(request);
 
+        ActionResponse response = ActionResponse.newBuilder()
+                .setSuccess(true)
+                .setMessage("Update successfully.")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 
     @Override
     public void moveDocument(MoveDocumentRequest request, StreamObserver<ActionResponse> responseObserver) {
+        documentService.moveDocument(request);
 
+        ActionResponse response = ActionResponse.newBuilder()
+                .setSuccess(true)
+                .setMessage("Move successfully.")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 
     @Override
     public void deleteDocument(DeleteDocumentRequest request, StreamObserver<ActionResponse> responseObserver) {
+        documentService.deleteDocument(request);
 
+        ActionResponse response = ActionResponse.newBuilder()
+                .setSuccess(true)
+                .setMessage("Delete successfully.")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 
     //Image
     @Override
     public void uploadImage(UploadImageRequest request, StreamObserver<UploadImageResponse> responseObserver) {
+        FileResponseDto file = fileService.uploadFile(request.getFolderPath(), request.getPublicId(), request.getFile());
 
+        UploadImageResponse response = UploadImageResponse.newBuilder()
+                .setUrl(file.getUrl())
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
