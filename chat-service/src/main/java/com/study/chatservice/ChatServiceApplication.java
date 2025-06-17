6a1e31998c06713ebb09fcbdd33d51c30470e9ca
Test_repo <22520992@gm.uit.edu.vn>
2025-06-17 16:21:12 +0700

    feat: add proto file and necessary packages

--- chat-service/src/main/java/com/study/chatservice/ChatServiceApplication.java
+++ chat-service/src/main/java/com/study/chatservice/ChatServiceApplication.java
@@ -2,8 +2,10 @@ package com.study.chatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.study.chatservice", "com.study.common"})
+@EnableDiscoveryClient
 public class ChatServiceApplication {
 
     public static void main(String[] args) {
