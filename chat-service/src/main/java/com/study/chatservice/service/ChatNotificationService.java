41448d46c6330e68f937327f7651d51093f246c0
Test_repo <22520992@gm.uit.edu.vn>
2025-06-18 00:52:08 +0700

    refactor: implement chat notification service

--- chat-service/src/main/java/com/study/chatservice/service/ChatNotificationService.java
+++ chat-service/src/main/java/com/study/chatservice/service/ChatNotificationService.java
@@ -1,7 +1,9 @@
 package com.study.chatservice.service;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ChatNotificationService {
-    void publishChatNotification(UUID teamId, int newMessageCount);
+    void increaseNewMessageCount(UUID teamId);
+    void publishChatNotification(UUID teamId, String teamName, int newMessageCount, List<UUID> receiverIds);
 }
--- chat-service/src/main/java/com/study/chatservice/service/impl/ChatNotificationServiceImpl.java
+++ chat-service/src/main/java/com/study/chatservice/service/impl/ChatNotificationServiceImpl.java
@@ -1,22 +1,44 @@
 package com.study.chatservice.service.impl;
 
 import com.study.chatservice.event.ChatEventPublisher;
-import com.study.chatservice.grpc.TeamServiceGrpcClient;
 import com.study.chatservice.service.ChatNotificationService;
+import com.study.common.events.Chat.MessageSentEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ChatNotificationServiceImpl implements ChatNotificationService {
-    private TeamServiceGrpcClient teamClient;
-    private ChatEventPublisher publisher;
-    //Handle with redis;
+    private final RedisTemplate<UUID, Integer> redisTemplate;
+    private final ChatEventPublisher publisher;
+    private final static String SEND_TOPIC = "message-sent";
 
     @Override
-    public void publishChatNotification(UUID teamId, int newMessageCount) {
+    public void increaseNewMessageCount(UUID teamId) {
+        Integer count = redisTemplate.opsForValue().get(teamId);
 
+        if (count == null) {
+            redisTemplate.opsForValue().set(teamId, 1);
+        }
+        else {
+            redisTemplate.opsForValue().set(teamId, count + 1);
+        }
+    }
+
+    @Override
+    public void publishChatNotification(UUID teamId, String teamName, int newMessageCount, List<UUID> receiverIds) {
+        MessageSentEvent event = MessageSentEvent.builder()
+                .teamId(teamId)
+                .teamName(teamName)
+                .newMessageCount(newMessageCount)
+                .receiverIds(receiverIds)
+                .build();
+
+        publisher.publishEvent(SEND_TOPIC, event);
+        redisTemplate.delete(teamId);
     }
 }
