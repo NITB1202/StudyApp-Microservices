0bedf8a3edb9fa1b534bbcadf76b6579ecaaf400
Test_repo <22520992@gm.uit.edu.vn>
2025-06-17 09:56:09 +0700

    feat: proto file and necessary packages

--- session-service/src/main/java/com/study/sessionservice/SessionServiceApplication.java
+++ session-service/src/main/java/com/study/sessionservice/SessionServiceApplication.java
@@ -2,8 +2,10 @@ package com.study.sessionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.study.sessionservice", "com.study.common"})
+@EnableDiscoveryClient
 public class SessionServiceApplication {
 
     public static void main(String[] args) {
