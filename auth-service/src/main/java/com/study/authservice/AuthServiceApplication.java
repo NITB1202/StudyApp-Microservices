a8e1ce7ba786e60a935445f0f59cd8fcb341fe2f
Test_repo <22520992@gm.uit.edu.vn>
2025-06-18 23:13:12 +0700

    feat: add proto file and necessary packages

--- auth-service/src/main/java/com/study/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/study/authservice/AuthServiceApplication.java
@@ -2,8 +2,10 @@ package com.study.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.study.authservice", "com.study.common"})
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
