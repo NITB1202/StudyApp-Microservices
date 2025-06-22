f009f99204eb3e8039dcf1bdf1da26ecb2b9f0f4
Test_repo <22520992@gm.uit.edu.vn>
2025-06-22 20:57:25 +0700

    refactor: remove .env library

--- auth-service/src/main/java/com/study/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/study/authservice/AuthServiceApplication.java
@@ -1,24 +1,14 @@
 package com.study.authservice;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(scanBasePackages = {"com.study.authservice", "com.study.common"})
 @EnableDiscoveryClient
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(AuthServiceApplication.class)
-                .initializers((ApplicationContextInitializer<ConfigurableApplicationContext>) context -> {
-                    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-                    dotenv.entries().forEach(entry ->
-                            System.setProperty(entry.getKey(), entry.getValue())
-                    );
-                })
-                .run(args);
+        SpringApplication.run(AuthServiceApplication.class, args);
     }
 }
